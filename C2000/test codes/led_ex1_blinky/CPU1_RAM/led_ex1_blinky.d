2d0d4f30568182bd7176d26bf09305efb5bcda3a
hossamadel7 <hossam.adel2013@yahoo.com>
2024-02-22 15:04:20 +0200

    led test to check working of C2000

---
+++ C2000/test codes/led_ex1_blinky/CPU1_RAM/led_ex1_blinky.d
@@ -0,0 +1,273 @@
+# FIXED
+
+led_ex1_blinky.obj: ../led_ex1_blinky.c
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/device_support/f28002x/common/include/driverlib.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_memmap.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/adc.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdbool.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_ti_config.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/linkage.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdint.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_stdint40.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/stdint.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/cdefs.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_types.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_types.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_stdint.h
+led_ex1_blinky.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_stdint.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_adc.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_asysctl.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_types.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/cpu.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/debug.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/asysctl.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/bgcrc.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_bgcrc.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/can.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_can.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/sysctl.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_nmi.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_sysctl.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/interrupt.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_ints.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_pie.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/clb.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_clb.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/cmpss.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_cmpss.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/cputimer.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_cputimer.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/dcc.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_dcc.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/dcsm.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_dcsm.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/dma.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_dma.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/ecap.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_ecap.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/epwm.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_epwm.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/eqep.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_eqep.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/erad.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_erad.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/flash.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_flash.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/fsi.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_fsi.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/hw_reg_inclusive_terminology.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/gpio.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_gpio.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_xint.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/xbar.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_clbxbar.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_epwmxbar.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_inputxbar.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_outputxbar.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_xbar.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/hrcap.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_hrcap.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/hrpwm.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_hrpwm.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/hrpwm.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/i2c.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_i2c.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/lin.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_lin.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/memcfg.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_memcfg.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/pin_map.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/pmbus.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_pmbus.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/pmbus_common.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/sci.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_sci.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/spi.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_spi.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/version.h
+led_ex1_blinky.obj: D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/driver_inclusive_terminology_mapping.h
+led_ex1_blinky.obj: syscfg/device.h
+led_ex1_blinky.obj: syscfg/clocktree.h
+led_ex1_blinky.obj: syscfg/board.h
+led_ex1_blinky.obj: syscfg/c2000ware_libraries.h
+
+../led_ex1_blinky.c:
+
+D:/C2000Ware_5_01_00_00/device_support/f28002x/common/include/driverlib.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_memmap.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/adc.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdbool.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/linkage.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_stdint40.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_types.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_types.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_stdint.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_adc.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_asysctl.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_types.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/cpu.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/debug.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/asysctl.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/bgcrc.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_bgcrc.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/can.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_can.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/sysctl.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_nmi.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_sysctl.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/interrupt.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_ints.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_pie.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/clb.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_clb.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/cmpss.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_cmpss.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/cputimer.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_cputimer.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/dcc.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_dcc.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/dcsm.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_dcsm.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/dma.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_dma.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/ecap.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_ecap.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/epwm.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_epwm.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/eqep.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_eqep.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/erad.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_erad.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/flash.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_flash.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/fsi.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_fsi.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/hw_reg_inclusive_terminology.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/gpio.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_gpio.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_xint.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/xbar.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_clbxbar.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_epwmxbar.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_inputxbar.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_outputxbar.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_xbar.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/hrcap.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_hrcap.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/hrpwm.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_hrpwm.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/hrpwm.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/i2c.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_i2c.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/lin.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_lin.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/memcfg.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_memcfg.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/pin_map.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/pmbus.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_pmbus.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/pmbus_common.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/sci.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_sci.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/spi.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/inc/hw_spi.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/version.h:
+
+D:/C2000Ware_5_01_00_00/driverlib/f28002x/driverlib/driver_inclusive_terminology_mapping.h:
+
+syscfg/device.h:
+
+syscfg/clocktree.h:
+
+syscfg/board.h:
+
+syscfg/c2000ware_libraries.h:
+
